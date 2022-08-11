cde03e03c4df3dc8de5bdef57a6a80fc8d4bc6a3
Dhruv-Paghdal <dhruvpaghdal2@gmail.com>
2022-08-11 17:49:23 +0530

    Admin Side Backend Completed

--- backend/routes/Password.js
+++ backend/routes/Password.js
@@ -1,23 +1,62 @@
-const accountSid = 'ACbe36619c3ef78d6b16cef80dc2fb0bc0'; 
-const authToken = '48723275faa4e26d774d04fdc205ab5e'; 
-import twilio from 'twilio';
+import nodemailer from 'nodemailer'
 import loginDB from "../models/Login.js";
-const client = twilio(accountSid, authToken); 
+import bcrypt from "bcrypt";
+import { uuid } from 'uuidv4'
 let otpCode;
 
-const sendOTP = (req,res) =>{
+const sendOTP = async(req,res) =>{
     try {
-        const phoneNumber  = req.body.number;
+        const { email } = req.body;
+        const userExist = await loginDB.findOne({email});
+        if(!userExist){
+            return res.status(404).json({Message:"No User Found"});
+        }
         otpCode=Math.floor(100000 + Math.random() * 900000);
-        client.messages 
-        .create({         
-           body: `Dear User, Your OTP is ${otpCode} for reseting your password. Thanks, quora Team.`,  
-           from:'+12283386259',
-           to: `+91${phoneNumber}` 
-         }) 
-        .then(message =>
-            res.status(200).json({Message:"OTP sent to your registered mobile number",MessageID:message.sid}) 
-        ); 
+        let testAccount = await nodemailer.createTestAccount();
+        const transporter = nodemailer.createTransport({
+            host: "smtp.ethereal.email",
+            port: 587,
+            secure: false, 
+            auth: {
+            user: testAccount.user, 
+            pass: testAccount.pass
+            }
+        });
+        const resetToken = uuid();
+        const updatedUser = await loginDB.findOneAndUpdate({email},{$set:{reset_token:resetToken,reset_otp:otpCode}},{new:true,upsert: false});
+        const mail = await transporter.sendMail({
+        from: '"Reset Password"', 
+        to: email, 
+        subject: "Reset Password - Team Quora.",
+        html:`<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>Reset Password</title>
+        </head>
+        <body>
+            Hi ${updatedUser.name},
+            <br>
+            There was a request to change your password!
+
+            If you did not make this request then please ignore this email.
+            <br>
+            <br>
+            Your OTP is <b>${otpCode}</b> for reseting your password.
+            <br>
+            <br>
+            Thanks, Quora Team.
+        </body>
+        </html>`
+        })
+        if(mail){
+            return res.status(200).json({Message: nodemailer.getTestMessageUrl(mail),resetToken});
+        }
+        else{
+            return res.status(500).json({Message:"Something Went Wrong"});
+        }
     } catch (error) {
         res.status(500).json(error.message);
     }
@@ -25,14 +64,21 @@ const sendOTP = (req,res) =>{
 
 const verifyOtpAndUpdate = async(req,res) =>{
     try {
-        const {user,otp,password} = req.body;
-        if(otp==otpCode){
-            const updatedUser = await loginDB.findOneAndUpdate({ phone: user.phone },{$set:{password}},{new:true});
+        const {token,otp,password} = req.body;
+        const userExist = await loginDB.findOne({reset_token:token})
+        if(!userExist){
+            return res.status(404).json({Message:"No User Found"});
+        }
+        if(otp==userExist.reset_otp){
+            const salt = bcrypt.genSaltSync(10);
+            const encrypted = bcrypt.hashSync(password, salt);
+            const updatedUser = await loginDB.findOneAndUpdate({reset_token:token},{$set:{password:encrypted}},{new:true});
             if(updatedUser){
-                res.send(200).json({Message:"Password Updated Successfully"}) 
+                await loginDB.findOneAndUpdate({reset_token:token},{$unset:{reset_token:"",reset_otp:""}},{new:true});
+                res.status(200).json({Message:"Password Updated Successfully"}) 
             }
             else{
-                res.send(400).json({Message:"Error in updating password"}) 
+                res.status(400).json({Message:"Error in updating password"}) 
             }
         } 
         else{
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,6 +6,7 @@ import {questionRouter,getData,getCategoryData} from "./Question.js";
 import {answerRouter,answerRouterWithImage} from "./Answer.js";
 import {loginRouter,registerRouter} from "./Login.js";
 import {sendOTP,verifyOtpAndUpdate} from "./Password.js"
+import {deleteQuestion,deleteAnswer} from "./Admin.js"
 
 
 // router.get("/", (req, res) => {
@@ -21,4 +22,6 @@ router.post("/register",registerRouter)
 router.post("/questions", questionRouter);
 router.post("/answers", answerRouter);
 router.post("/answers/image", store.single('answerImage'),answerRouterWithImage);
+router.delete("/admin/delete/question/:id",deleteQuestion);
+router.delete("/admin/delete/answer/:id",deleteAnswer);
 export default router;
